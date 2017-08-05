80eb399449909306a725da49cbb4ef397d6152ca
Tomasz Martyński <tomek@martynski.pl>
2017-08-05 23:22:42 +0200

    Resize text in CalcScreen automatically

--- src/Calculator/CalcScreen.js
+++ src/Calculator/CalcScreen.js
@@ -1,33 +1,38 @@
 import React, { Component } from 'react';
+import { Textfit } from 'react-textfit';
+
+const MAX_FONT_SIZE = 45;
 
 class CalcScreen extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      opacity: 1
+        this.state = {
+            opacity: 1
+        }
     }
-  }
 
-  componentWillMount() {
-    this.interval = setInterval(() => this.setState({ opacity: Number(!this.state.opacity) }), 500);
-  }
+    componentWillMount() {
+        this.interval = setInterval(() => this.setState({ opacity: Number(!this.state.opacity) }), 500);
+    }
 
-  componenWillUnmount() {
-    clearInterval(this.interval);
-  }
-  
-  render() {
-    return (
-      <div className="calc-screen">
-        <div className="calc-operation">{this.props.calcTopText}</div>
-        <div className="calc-typed">
-          <span className="calc-result">{this.props.calcBottomText}</span>
-          <span className="blink-me" style={{opacity: this.state.opacity, transition: "opacity .5s"}}>_</span>
-        </div>
-      </div>
-    );
-  }
+    componenWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        return (
+            <div className="calc-screen">
+                <div className="calc-operation">{this.props.calcTopText}</div>
+                <div className="calc-typed">
+                    <Textfit mode="single" max={MAX_FONT_SIZE}>
+                        <span className="calc-result">{this.props.calcBottomText}</span>
+                        <span className="blink-me" style={{ opacity: this.state.opacity, transition: "opacity .5s" }}>_</span>
+                    </Textfit>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default CalcScreen;
